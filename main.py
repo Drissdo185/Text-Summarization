af66c2d9036764dc74cb2a42fd4428086a4029f2
Đỗ Thành Đạt <107539933+Drissdo185@users.noreply.github.com>
2024-02-19 20:53:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ app = FastAPI(root_path="/txtapp-service")
 
 
 @app.get("/Text_Summarization")
-def text_summarization(Text: Optional[str] = None):
+def text_summarization(text: Optional[str] = None):
     results = {"Mlops": [{"Author": "DrissDo"}]}
-    if Text:
+    if text:
         # Use the summarizer to summarize the text
-        summary = summarizer(Text, max_length=130, min_length=30, do_sample=False)
+        summary = summarizer(text, max_length=130, min_length=30, do_sample=False)
         results.update({"Text Summarization ": summary})
     return results
